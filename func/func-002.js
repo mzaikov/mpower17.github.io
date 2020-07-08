f302f6bd900b224afdce9bf812679a90da8c45f6
Maksim <maxim.zaykov97@gmail.com>
2020-07-08 20:47:40 +0300

    Updated main html, fixed func labs

--- func/func-002.js
+++
@@ -1,7 +0,0 @@
-const mixin = function(){ 
-    return this * this; 
-};
-
-mixin.apply(5); 
-// или mixin.call(5);
-// или mixin.bind(5)();
--- func/func-003.js
+++
@@ -1,2 +0,0 @@
-let {default: exportedFunc} = await import('https://kodaktor.ru/j/func_003_export');
-exportedFunc.bind(null, 5)();
